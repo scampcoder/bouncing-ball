312ca5ac0ff5bb4334250ba6802b115b3a06a0e2
Victoria Kee <victoriakee95@gmail.com>
2020-05-12 11:46:59 -0500

    Add inital JS and drawBall

---
+++ app.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('mycanvas');
+const ctx = canvas.getContext("2d");//store our drawing on canvas
+
+function drawBall() {
+  ctx.beginPath();//start drawing
+  ctx.arc(220, 140, 20, 0, Math.PI*2, false)
+  //arc(x coord at center, y coord at center, radius, starting angle, ending angle, counterclockwise)
+  ctx.fillStyle('red');//give color to ball
+  ctx.fill();//actually fills the ball
+  ctx.closePath();//end drawing
+}
