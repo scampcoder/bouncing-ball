1d74587e8a4140db0bae70a9102dcc303ff35c41
Victoria Kee <victoriakee95@gmail.com>
2020-05-12 11:57:10 -0500

    Provide initial ball movement

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext("2d");//store our drawing on canvas
+let x = canvas.width/2;
+let y = canvas.height - 30; //new x and y will move ball
+let dx = 2;
+let dy = -2;
 
 function drawBall() {
   ctx.beginPath();//start drawing
-  ctx.arc(220, 140, 20, 0, Math.PI*2, false)
+  ctx.arc(x, y, 10, 0, Math.PI*2)
   //arc(x coord at center, y coord at center, radius, starting angle, ending angle, counterclockwise)
   ctx.fillStyle = 'red';//give color to ball
   ctx.fill();//actually fills the ball
   ctx.closePath();//end drawing
+  x += dx; //define new coords for next frame
+  y += dy;
 }
 setInterval(drawBall, 10)//will run drawBall every 10 milliseconds
