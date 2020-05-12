339331ca4649abb31e06fd31de994198ce133129
Victoria Kee <victoriakee95@gmail.com>
2020-05-12 11:50:12 -0500

    Fix fillStyle typo for drawBall

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ function drawBall() {
   ctx.beginPath();//start drawing
   ctx.arc(220, 140, 20, 0, Math.PI*2, false)
   //arc(x coord at center, y coord at center, radius, starting angle, ending angle, counterclockwise)
-  ctx.fillStyle('red');//give color to ball
+  ctx.fillStyle = 'red';//give color to ball
   ctx.fill();//actually fills the ball
   ctx.closePath();//end drawing
 }
+setInterval(drawBall, 10)//will run drawBall every 10 milliseconds
