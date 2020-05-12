cf682d8f9c0fbe5f9fc449caaa24f50364e99e51
Victoria Kee <victoriakee95@gmail.com>
2020-05-12 13:53:51 -0500

    Implement collision detection for bouncing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext("2d");//store our drawing on canvas
+const ballRadius = 10;
 let x = canvas.width/2;
 let y = canvas.height - 30; //new x and y will move ball
 let dx = 2;
@@ -7,7 +8,7 @@ let dy = -2;
 
 function drawBall() {
   ctx.beginPath();//start drawing
-  ctx.arc(x, y, 10, 0, Math.PI*2)
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2)
   //arc(x coord at center, y coord at center, radius, starting angle, ending angle, counterclockwise)
   ctx.fillStyle = 'red';//give color to ball
   ctx.fill();//actually fills the ball
@@ -17,6 +18,12 @@ function drawBall() {
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height) //clear canvas each frame
   drawBall();
+  if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){ //bounce off L or R sides
+    dx = -dx; //go the opposite way
+  }
+  if(y + dy > canvas.width - ballRadius || y + dy < ballRadius){ //bounce off top or bottom
+    dy = -dy; //go the opposite way
+  }
   x += dx; //define new coords for next frame
   y += dy;
 }
